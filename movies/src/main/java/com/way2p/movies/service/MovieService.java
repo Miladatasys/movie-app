b2512375b33a1aabb533960ae43969afb9dfd59b
Mila <miladata.sys@gmail.com>
2024-01-11 00:14:00 -0300

    adding repository, controller and service

---
+++ movies/src/main/java/com/way2p/movies/service/MovieService.java
@@ -0,0 +1,20 @@
+package com.way2p.movies.service;
+
+import com.way2p.movies.models.Movie;
+import com.way2p.movies.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+}
+    
